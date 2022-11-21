aaac4421d01cfa0e47c68926a77d0fa71094c9fb
Malik <snapfirecookie@gmail.com>
2022-11-21 23:58:07 +0500

    Added 2nd type of menu

--- src/router/main.js
+++ src/router/main.js
@@ -1,6 +1,3 @@
-// import map_route_default from './map_route';
-// import map_route_redirect from './map_route';
-// import profile_route_default from '.profile_route';
 import { createRouter, createWebHistory } from 'vue-router';
 import MaainComponent from '../components/main_component.vue';
 import ProfileComponent from '../components/profile_component.vue';
